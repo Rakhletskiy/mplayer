6e0211100edfda0cdfd22063e9c60fa7b6c21763
Yuriy Rakhletskyy <rakhletskyy22@gmail.com>
2019-11-02 18:39:06 +0200

    Updates

---
+++ precache-manifest.9617ff836ce4cd15fca12e7c07a2f9a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b2c11eaf46d9ee6417be40ff85b405",
+    "url": "/mplayer/index.html"
+  },
+  {
+    "revision": "6a8afd4a55ee6e5edc43",
+    "url": "/mplayer/static/css/2.4e100073.chunk.css"
+  },
+  {
+    "revision": "f36d73d93b930673f125",
+    "url": "/mplayer/static/css/main.9ceeeea1.chunk.css"
+  },
+  {
+    "revision": "6a8afd4a55ee6e5edc43",
+    "url": "/mplayer/static/js/2.2dc0c4d4.chunk.js"
+  },
+  {
+    "revision": "f36d73d93b930673f125",
+    "url": "/mplayer/static/js/main.a82a4a41.chunk.js"
+  },
+  {
+    "revision": "d995b22f8d19eb0aa1bb",
+    "url": "/mplayer/static/js/runtime-main.22228eed.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mplayer/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mplayer/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mplayer/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mplayer/static/media/slick.f97e3bbf.svg"
+  }
+]);
